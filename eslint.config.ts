0af915da00336dbab80500ce4d27e68f4d517ff4
tooyoungtobeloved <1246755863@qq.com>
2025-01-26 15:05:59 +0800

    feat:init project

---
+++ eslint.config.ts
@@ -0,0 +1,24 @@
+import pluginVue, { rules } from 'eslint-plugin-vue'
+console.log(pluginVue.configs['flat/essential'])
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  skipFormatting,
+)
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+// import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>123</header>
+</template>
